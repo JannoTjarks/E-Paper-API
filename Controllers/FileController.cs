520d357e14a9567539b9251dca591ac8a4d9b483
root <root@h2902020.stratoserver.net>
2021-03-20 01:00:53 +0100

    First commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace e_paper_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> dirs = Directory.GetFiles(@"/e-paper/").ToList();
+            return dirs;
+        }
+    }
+}
